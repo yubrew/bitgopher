42b4dffd2dd451b7285980e486c0596fe23a48f0
John Y <johnyu2@gmail.com>
2013-05-30 23:53:42 -0400

    add parsed and is_transaction flags to messages

---
+++ db/migrate/20130531034909_create_messages.rb
@@ -0,0 +1,16 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :message_id, null: false
+      t.string :sender, null: false
+      t.string :sender_id, null: false
+      t.text :content, null: false
+      t.string :message_type, null: false
+      t.text :full_message, null: false
+      t.boolean :parsed, default: false
+      t.boolean :is_transaction, default: false
+
+      t.timestamps
+    end
+  end
+end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,8 +3,12 @@
 FactoryGirl.define do
   factory :message do
     message_id "MyString"
-    sender_id "MyString"
     sender "MyString"
+    sender_id "MyString"
     content "MyText"
+    message_type "MyString"
+    full_message "MyText"
+    parsed false
+    is_transaction false
   end
 end
