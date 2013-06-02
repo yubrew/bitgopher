9c7708991b66f4f1a740c4271315cf04550fae5b
John Y <johnyu2@gmail.com>
2013-06-02 14:03:03 -0400

    create bit coin address for user (not functional yet)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :balance, :btc_address, :confirmed, :handle, :messaging_id
   validates :balance, :btc_address, :confirmed, :handle, :messaging_id, presence: true
+  validates :handle, uniqueness: true
   has_many :transactions
   has_many :bitcoin_transactions
+  before_validation :get_bitcoin_address
 
   def pending_sent_transactions
     self.transactions
@@ -10,5 +12,15 @@ class User < ActiveRecord::Base
 
   def available_balance
     self.balance - pending_sent_transactions
+  end
+
+  private
+  def get_bitcoin_address
+    new_bitcoin_address = bitcoind.getnewaddress(handle)
+    if new_bitcoin_address == 'error'
+      nil
+    else
+      self.btc_address = new_bitcoin_address
+    end
   end
 end
