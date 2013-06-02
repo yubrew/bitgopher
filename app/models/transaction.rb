c185967c125eb4457db2f0b316c1ac0e58e9f677
John Y <johnyu2@gmail.com>
2013-06-02 14:02:41 -0400

    add custom validator for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,9 +4,14 @@ class Transaction < ActiveRecord::Base
   belongs_to :from_user, class_name: 'User'
   belongs_to :to_user, class_name: 'User'
   validates :amount_in_btc, :from_user, :message_id, :status, :to_user, presence: true
-  before_validation :check_for_positive_balance
+  before_validation :check_duplicate_transaction, :check_for_positive_balance
 
   private
+  def check_duplicate_transaction
+    t = Transaction.where(from_user: self.from_user, to_user: self.to_user, message: :message_id)
+    t.blank?
+  end
+
   def check_for_positive_balance
     self.from_user.available_balance - self.amount_in_btc >= 0.0
   end
